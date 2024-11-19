731518418a837ffb0e313b96a9d3607a2b9dc7d3
JacobProut <Jprout@wgu.edu>
2024-11-18 20:18:34 -0600

    Refactoring

--- src/main/java/com/jacobpmods/neomod/item/custom/curiosItems/SoulSplitNecklace.java
+++ src/main/java/com/jacobpmods/neomod/item/custom/curiosItems/SoulSplitNecklace.java
@@ -1,15 +1,11 @@
 package com.jacobpmods.neomod.item.custom.curiosItems;
 
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
-
-import java.util.UUID;
 
 public class SoulSplitNecklace extends Item implements ICurioItem {
 
