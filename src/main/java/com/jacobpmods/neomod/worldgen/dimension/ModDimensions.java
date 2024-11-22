b6771fb3127d87f3dfab6b40ff98e3d4b3721a43
JacobProut <Jprout@wgu.edu>
2024-11-22 17:47:24 -0600

    Fixed Biome Generation

--- src/main/java/com/jacobpmods/neomod/worldgen/dimension/ModDimensions.java
+++ src/main/java/com/jacobpmods/neomod/worldgen/dimension/ModDimensions.java
@@ -68,9 +68,16 @@ public class ModDimensions {
         NoiseBasedChunkGenerator noiseBasedChunkGenerator = new NoiseBasedChunkGenerator(
                 MultiNoiseBiomeSource.createFromList(
                         new Climate.ParameterList<>(List.of(Pair.of(
-                                        Climate.parameters(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F), biomeRegistry.getOrThrow(ModBiomes.GHOSTLY_BIOME)),
+                                        //1: Temperature(Controls the "heat" of the biome. Lower values represent colder areas (e.g., snowy tundras), while higher values represent warmer areas (e.g., deserts))
+                                        //2: Humidity(Determines the wetness or dryness of the biome. Lower values correspond to dry biomes (e.g., deserts), while higher values correspond to wet biomes (e.g., jungles, swamps))
+                                        //3: Continentalness(Controls the distance from an ocean. Low values correspond to oceanic areas, while high values correspond to inland or central areas.)
+                                        //4: Erosion(Affects the steepness or flatness of terrain. Low values favor flatter areas like plains or plateaus, while high values favor more eroded areas like cliffs or mountains.)
+                                        //5: Weirdness(Used to add variation to biome placement by introducing "weird" or unexpected biome combinations. It is often used for biomes like mushroom fields or other rare/unique terrain.)
+                                        //6: Offset( Acts as a multiplier or bias to make certain biomes more likely. This value typically serves as a tiebreaker or an adjustment to ensure a desired biome is selected.)
+                                        //7: Noise Point Weight(Affects how this particular set of parameters influences biome placement when blended with others. Higher values increase the "weight" or significance of this parameter set in the overall biome map.)
+                                        Climate.parameters(0.0F, 0.0F, 0.2F, 0.0F, 0.0F, 0.0F, 0.0F), biomeRegistry.getOrThrow(ModBiomes.GHOSTLY_BIOME)),
                                 Pair.of(
-                                        Climate.parameters(0.1F, 0.2F, 0.0F, 0.2F, 0.0F, 0.0F, 0.0F), biomeRegistry.getOrThrow(ModBiomes.BLOOD_GARDEN_BIOME))
+                                        Climate.parameters(0.1F, 0.2F, 0.5F, 0.2F, 0.0F, 0.0F, 0.0F), biomeRegistry.getOrThrow(ModBiomes.BLOOD_GARDEN_BIOME))
                                 /*Pair.of(
                                         Climate.parameters(0.3F, 0.6F, 0.1F, 0.1F, 0.0F, 0.0F, 0.0F), biomeRegistry.getOrThrow(Biomes.OCEAN)),
                                 Pair.of(
