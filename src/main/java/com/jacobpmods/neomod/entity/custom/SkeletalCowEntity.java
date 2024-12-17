2a211824f467a3516d607fe312f59a18dab9b25d
JacobProut <Jprout@wgu.edu>
2024-12-17 01:01:55 -0600

    On death, Skeletal Cow drops undead beef

--- src/main/java/com/jacobpmods/neomod/entity/custom/SkeletalCowEntity.java
+++ src/main/java/com/jacobpmods/neomod/entity/custom/SkeletalCowEntity.java
@@ -1,18 +1,37 @@
 package com.jacobpmods.neomod.entity.custom;
 
 import com.jacobpmods.neomod.entity.ModEntities;
+import com.jacobpmods.neomod.item.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemUtils;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
 public class SkeletalCowEntity extends Animal {
@@ -40,10 +59,49 @@ public class SkeletalCowEntity extends Animal {
     //will need to change to match cows + extra health
     public static AttributeSupplier.Builder createAttributes() {
         return Animal.createLivingAttributes()
-                .add(Attributes.MAX_HEALTH, 200)
-                .add(Attributes.MOVEMENT_SPEED, 0.250)
+                .add(Attributes.MAX_HEALTH, 20)
+                .add(Attributes.MOVEMENT_SPEED, 0.2F)
                 .add(Attributes.FOLLOW_RANGE, 240);
     }
+
+    @Override
+    public InteractionResult mobInteract(Player p_28298_, InteractionHand p_28299_) {
+        ItemStack itemstack = p_28298_.getItemInHand(p_28299_);
+        if (itemstack.is(Items.BUCKET) && !this.isBaby()) {
+            p_28298_.playSound(SoundEvents.COW_MILK, 1.0F, 1.0F);
+            ItemStack itemstack1 = ItemUtils.createFilledResult(itemstack, p_28298_, Items.MILK_BUCKET.getDefaultInstance());
+            p_28298_.setItemInHand(p_28299_, itemstack1);
+            return InteractionResult.sidedSuccess(this.level().isClientSide);
+        } else {
+            return super.mobInteract(p_28298_, p_28299_);
+        }
+    }
+
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return SoundEvents.COW_AMBIENT;
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource p_28306_) {
+        return SoundEvents.COW_HURT;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.COW_DEATH;
+    }
+
+    @Override
+    protected void playStepSound(BlockPos p_28301_, BlockState p_28302_) {
+        this.playSound(SoundEvents.COW_STEP, 0.15F, 1.0F);
+    }
+
+    @Override
+    protected float getSoundVolume() {
+        return 0.4F;
+    }
+
 
 
     @Nullable
@@ -56,7 +114,17 @@ public class SkeletalCowEntity extends Animal {
         super.defineSynchedData(builder);
     }
 
-  /*  private void setupAnimationStates() {
+    @Override
+    protected void dropCustomDeathLoot(ServerLevel serverLevel, DamageSource damageSource, boolean recentlyHit) {
+        // Call the parent method to handle equipment drops
+        super.dropCustomDeathLoot(serverLevel, damageSource, recentlyHit);
+
+        // Add custom drops
+        this.spawnAtLocation(ModItems.UNDEAD_BEEF, this.random.nextInt(3) + 1); // Drop 1-3 bones
+
+    }
+
+    /*  private void setupAnimationStates() {
         if(this.idleAnimationTimeout <=0) {
             this.idleAnimationTimeout = 20;
         }
