4818e391efc0e127c5920825ba3d811e5096eb0b
JacobProut <Jprout@wgu.edu>
2025-01-17 13:39:03 -0600

    Skeletal Guardian now has translucent Texture!!

--- src/main/java/com/jacobpmods/neomod/entity/client/skeletal/guardian/SkeletalGuardianRender.java
+++ src/main/java/com/jacobpmods/neomod/entity/client/skeletal/guardian/SkeletalGuardianRender.java
@@ -5,11 +5,13 @@ import com.jacobpmods.neomod.entity.client.ModModelLayers;
 import com.jacobpmods.neomod.entity.custom.SkeletalGuardianEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
 
-public class SkeletalGuardianRender extends MobRenderer<SkeletalGuardianEntity, SkeletalGuardianModel> {
+public class SkeletalGuardianRender extends LivingEntityRenderer<SkeletalGuardianEntity, SkeletalGuardianModel> {
     public SkeletalGuardianRender(EntityRendererProvider.Context context) {
         super(context, new SkeletalGuardianModel(context.bakeLayer(ModModelLayers.SKELETAL_GUARDIAN)), 0.4f);
     }
@@ -28,5 +30,11 @@ public class SkeletalGuardianRender extends MobRenderer<SkeletalGuardianEntity, 
         super.render(skeletalGuardianEntity, pEntityYaw, pPartialTicks, pPoseStack, pBuffer, pPackedLight);
 
 
+    }
+
+    @Nullable
+    @Override
+    public RenderType getRenderType(SkeletalGuardianEntity entity, boolean p_115323_, boolean p_115324_, boolean p_115325_) {
+        return RenderType.ENTITY_TRANSLUCENT.apply(getTextureLocation(entity), false);
     }
 }
